0c4073330f16529052c4c4db9c468327db6d9f56
J Taylor OConnor <jtomusic@gmail.com>
2015-05-03 11:42:16 -0500

    initial commit

---
+++ src/main/java/jto/processing/VideoTestSketch.java
@@ -0,0 +1,30 @@
+package jto.processing;
+
+import processing.core.PApplet;
+import processing.video.Capture;
+
+public class VideoTestSketch extends PApplet {
+
+    private Capture capture;
+
+    @Override
+    public void setup() {
+        size(1920, 1080);
+        String[] cameras = Capture.list();
+        for(String cam : cameras) {
+            println(cam);
+        }
+
+        capture = new Capture(this, width, height, cameras[0]);
+    }
+
+    @Override
+    public void draw() {
+        image(capture.get(), 0, 0);
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("java.library.path", "/opt/dev/processing-deps/video/macosx64");
+        PApplet.main(new String[]{VideoTestSketch.class.getName()});
+    }
+}
