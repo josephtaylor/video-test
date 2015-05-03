a503a4602e7a97ac5ec57d9a2b231e6085d75d79
joconnor <jtomusic@gmail.com>
2015-05-03 14:10:25 -0500

    fixing up sketch

--- src/main/java/jto/processing/VideoTestSketch.java
+++ src/main/java/jto/processing/VideoTestSketch.java
@@ -8,23 +8,38 @@ public class VideoTestSketch extends PApplet {
     private Capture capture;
 
     public static void main(String[] args) {
-        System.setProperty("java.library.path", "/opt/dev/processing-deps/video/macosx64");
-        PApplet.main(new String[]{VideoTestSketch.class.getName()});
+		// better idea to specify these as VM args
+		// included here for simplicity
+        System.setProperty("jna.library.path", "C:\\dev\\processing-deps\\video\\windows64");
+        System.setProperty("gstreamer.library.path", "C:\\dev\\processing-deps\\video\\windows64");
+        System.setProperty("gstreamer.plugin.path", "C:\\dev\\processing-deps\\video\\windows64\\plugins");
+
+		for (String name : System.getProperties().stringPropertyNames()) {
+			String prop = System.getProperty(name);
+			println(name + ":\t" + prop);
+		}
+
+        PApplet.main(new String[] { VideoTestSketch.class.getName() });
     }
 
     @Override
     public void setup() {
-        size(1920, 1080);
+        size(640, 480);
         String[] cameras = Capture.list();
         for (String cam : cameras) {
             println(cam);
         }
 
-        capture = new Capture(this, width, height, cameras[0]);
+		// my camera is at index 1. Change it to be the camera you need.
+        capture = new Capture(this, width, height, cameras[1]);
+		capture.start();
     }
 
     @Override
     public void draw() {
+		if (capture.available()) {
+			capture.read();
+		}
         image(capture.get(), 0, 0);
     }
 }
